ba47098b830d1953393ced0af99426e50f8d10ef
fetiu <k9380450@gmail.com>
2024-10-01 05:24:49 +0900

    feat: add first server with preview and photo API

---
+++ camera.py
@@ -0,0 +1,56 @@
+import os, subprocess
+
+# Each frame is about 180 KB
+READ_SIZE = 100 * 1024
+SOI = b'\xff\xd8'
+EOI = b'\xff\xd9'
+
+# Kill existing gphoto2 processes to properly detect camera
+def reset():
+    os.system("pkill gphoto2")
+
+def command(arg):
+    command = ['gphoto2', arg]
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    if result.returncode == 0:
+        return result.stdout.decode('utf-8');
+
+def capture_image():
+    reset()
+    command = ['gphoto2', '--capture-image-and-download', '--stdout']
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    if result.returncode == 0:
+        return result.stdout;
+
+class MjpegStream:
+    def __init__(self, timeout=None):
+        reset()
+        # Start capturing video from gphoto2 and send it over pipe
+        command = ['gphoto2', '--capture-movie', '--stdout']
+        if timeout:
+            command[1] += f'={timeout}s'
+        self.process = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=10**8)
+        self.buf = b''
+
+    def get_frame(self):
+        while True:
+            # Read in larger chunks to avoid too many reads, but ensure full frames
+            chunk = self.process.stdout.read(READ_SIZE)
+            if not chunk:
+                break
+            self.buf += chunk
+
+            # Look for a complete JPEG frame (starting with FFD8 and ending with FFD9)
+            start = self.buf.find(SOI)  # Start of JPEG frame
+            end = self.buf.find(EOI)    # End of JPEG frame
+
+            if start != -1 and end != -1:
+                end += len(EOI)
+                frame = self.buf[start:end]
+                self.buf = self.buf[end:]  # Remove the processed frame from the
+                return frame
+
+    def __del__(self):
+        self.process.terminate()
+        self.process.wait()
+
---
+++ server.py
@@ -0,0 +1,41 @@
+#!/bin/python
+
+from flask import Flask, Response, render_template, request
+import camera
+
+app = Flask(__name__)
+
+def generate_stream(timeout):
+    mjpg = camera.MjpegStream(timeout)
+    while True:
+        frame = mjpg.get_frame()
+        if frame:
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    return Response(generate_stream(request.args.get('timeout')),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/photo', methods=["GET"])
+def photo():
+    return Response(camera.capture_image(), mimetype='image/jpeg')
+
+@app.route('/reset', methods=["GET"])
+def reset():
+    camera.reset()
+    return Response('Camera has been reset.')
+
+@app.route('/cmd', methods=["GET"])
+def command():
+    text = camera.command(request.args.get('arg'))
+    return Response(text, mimetype='text/plain')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
